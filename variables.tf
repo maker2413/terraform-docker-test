0a72bc04d3ffa032c9acad6e3625b48f14a46fbe
maker2413 <maker2413@gmail.com>
2021-01-18 05:22:40 -0800

    using workspaces now

--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
-locals {
-  container_count = length(var.ext_port)
+variable "image" {
+  type = map
+  description = "image for container"
+  default = {
+    dev = "nodered/node-red:latest"
+    prod = "nodered/node-red:latest-minimal"
+  }
 }
 
 variable "int_port" {
@@ -13,10 +18,19 @@ variable "int_port" {
 }
 
 variable "ext_port" {
-  type = list
+  type = map
 
   validation {
-    condition = max(var.ext_port...) <= 65535 && min(var.ext_port...) > 0
+    condition = max(var.ext_port["dev"]...) <= 65535 && min(var.ext_port["dev"]...) >= 1980
     error_message = "The external port must be in the valid port range 0 - 65535."
   }
+
+  validation {
+    condition = max(var.ext_port["prod"]...) < 1980 && min(var.ext_port["prod"]...) >= 1880
+    error_message = "The external port must be in the valid port range 0 - 65535."
+  }
+}
+
+locals {
+  container_count = length(lookup(var.ext_port, terraform.workspace))
 }
