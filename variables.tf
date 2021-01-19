1b8c936acdafac7ffa4c354b842e256f269d2e69
maker2413 <maker2413@gmail.com>
2021-01-19 01:07:49 -0800

    ran terraform fmt -recursive

--- variables.tf
+++ variables.tf
@@ -1,22 +1,22 @@
 variable "image" {
-  type = map
+  type        = map(any)
   description = "image for container"
   default = {
     nodered = {
-      dev = "nodered/node-red:latest"
+      dev  = "nodered/node-red:latest"
       prod = "nodered/node-red:latest-minimal"
     }
     influxdb = {
-      dev = "quay.io/influxdb/influxdb:v2.0.2"
+      dev  = "quay.io/influxdb/influxdb:v2.0.2"
       prod = "quay.io/influxdb/influxdb:v2.0.2"
     }
     grafana = {
-      dev = "grafana/grafana"
+      dev  = "grafana/grafana"
       prod = "grafana/grafana"
     }
   }
 }
 
 variable "ext_port" {
-  type = map
+  type = map(any)
 }
