cc55e6bf6c1631a0737582637fe6ba1f287494aa
maker2413 <maker2413@gmail.com>
2021-01-19 01:06:49 -0800

    cleaned up repo

--- container/outputs.tf
+++ container/outputs.tf
@@ -1,13 +1,3 @@
-# output "container-name" {
-#   value       = docker_container.nodered_container.name
-#   description = "The name of the container"
-# }
-
-# output "ip-address" {
-#   value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], [i.ports[0]["external"]])]
-#   description = "The IP address and external port of the container"
-# }
-
 output "application_access" {
   value = {for x in docker_container.app_container[*]: x.name => join(":", [x.ip_address], x.ports[*]["external"])}
 }
